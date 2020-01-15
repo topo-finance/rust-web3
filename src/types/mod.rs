87c16dc1bee8bcd688629ddb4b8255e65b89fc8d
brockelmore <31553173+brockelmore@users.noreply.github.com>
2020-01-15 13:18:32 -0500

    Add U512

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -35,7 +35,7 @@ pub use self::traces::{
 pub use self::transaction::{RawTransaction, Receipt as TransactionReceipt, Transaction};
 pub use self::transaction_id::TransactionId;
 pub use self::transaction_request::{CallRequest, TransactionCondition, TransactionRequest};
-pub use self::uint::{H128, H160, H2048, H256, H512, H520, H64, U128, U256, U64};
+pub use self::uint::{H128, H160, H2048, H256, H512, H520, H64, U128, U256, U512, U64};
 pub use self::work::Work;
 
 /// Address
